4e2fb5c0d21834e28cfb77c1ec05f45c6e6e7631
esmaimeci <imeciesma@gmail.com>
2024-02-26 23:06:30 +0300

    first commit

---
+++ FoodyTekmerWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.title1 = "Minimalist Giyim";
+        
+
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialPageHeader()
+        {
+            return PartialView();
+        }
+    }
+}
